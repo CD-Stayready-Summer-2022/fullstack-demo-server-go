09fb4baeaf8e7feb7bd2ff2aad4ea0f823ebb566
Tamajong Nchukwi <tjnc@Tamajongs-MacBook-Pro-2.local>
2022-07-14 11:59:19 -0400

    service

--- src/test/java/com/example/fullstackdemo/EmployeeServiceTest.java
+++ src/test/java/com/example/fullstackdemo/EmployeeServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.fullstackdemo;
 
+import com.example.fullstackdemo.domain.core.exceptions.ResourceNotFoundException;
 import com.example.fullstackdemo.domain.employee.models.Employee;
 import com.example.fullstackdemo.domain.employee.repos.EmployeeRepo;
 import com.example.fullstackdemo.domain.employee.services.EmployeeService;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 @SpringBootTest()
 @ExtendWith(SpringExtension.class)
@@ -33,11 +36,30 @@ public class EmployeeServiceTest {
     }
 
     @Test
-    public void getByIdTest(){
+    public void getByIdTest() throws ResourceNotFoundException {
         Employee mockEmployee = new Employee("first" , "name", "email");
         mockEmployee.setId(1l);
-        BDDMockito.doReturn(mockEmployee).when(employeeRepo).findById(1l);
+        BDDMockito.doReturn(Optional.of(mockEmployee)).when(employeeRepo).findById(1l);
         Employee actual = employeeService.getById(1l);
+        Assertions.assertEquals(mockEmployee, actual);
+    }
+    @Test
+    public void getByIdTest01() throws ResourceNotFoundException {
+       BDDMockito.when(employeeRepo.findById(1l)).thenReturn(Optional.empty());
+       Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+           employeeService.getById(1l);
+       });
+    }
+
+    @Test
+    public void updateTest() throws ResourceNotFoundException {
+        Employee mockEmployee = new Employee("first" , "name", "email");
+        mockEmployee.setId(1l);
+        BDDMockito.doReturn(Optional.of(mockEmployee)).when(employeeRepo).findById(1l);
+        mockEmployee.setFirstName("newName");
+        BDDMockito.doReturn(mockEmployee).when(employeeRepo).save(mockEmployee);
+        Employee details = new Employee("newName", "name", "email");
+        Employee actual = employeeService.update(1L, details);
         Assertions.assertEquals(mockEmployee, actual);
     }
 }
